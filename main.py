8548f55fbc776bc04d450253068362c6cf6bd617
GreatGirya <kukumbos05@gmail.com>
2020-10-20 17:02:39 +1000

    -

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ W = 480
 H = 360
 SILVER = (192, 192, 192)
 BLACK = (0, 0, 0)
-numeral=''
-move=1
-block=0
-start=1
-OUTSIDE_BG=(0,-100)
+numeral = ''
+move = 1
+block = 0
+start = 1
+OUTSIDE_BG = (0, -100)
 
 pygame.init()
 pygame.display.set_caption('Угадай число')
 pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((W, H))
-# Arial
-font = pygame.font.SysFont('Arial',28,True,False)
-font_box = pygame.Surface((W-20,font.get_height()))
-font_rect = font_box.get_rect(center=(W//2,H - font.get_height()))
-font2 = pygame.font.SysFont('Arial',14,False,True)
+
+font = pygame.font.SysFont('Arial', 28, True, False)
+font_box = pygame.Surface((W - 20, font.get_height()))
+font_rect = font_box.get_rect(center=(W // 2, H - font.get_height()))
+font2 = pygame.font.SysFont('Arial', 14, False, True)
 
 path = os.path.dirname(os.path.abspath(__file__))
 bg = pygame.image.load(os.path.join(path, 'Image/room.png'))
@@ -38,27 +38,55 @@ dialog = pygame.image.load(os.path.join(path, 'Image/dialog.png'))
 dialog_rect = dialog.get_rect()
 dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
 dialog_dog_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
-# (┛`д´)┛
-# _(OwO_)   \(OwO\)    \(OwO)/   (/OwO)/    (_OwO)_
-def dialog(text,pos,owl_text):
+
+
+def dialogs(text, pos, owl_text):
     screen.blit(dialog, pos)
-    screen.blit(font2.render(text,True,BLACK), (pos[0]+5,pos[1]+5))
-    dialog_owl_pos = (owl_rect.x,owl_rect.y - dialog_rect.h)
+    screen.blit(font2.render(text, True, BLACK), (pos[0] + 5, pos[1] + 5))
+    dialog_owl_pos = (owl_rect.x, owl_rect.y - dialog_rect.h)
     screen.blit(dialog, dialog_owl_pos)
-    screen.blit(font2.render(owl_text,True,BLACK), (dialog_owl_pos[0]+5,dialog_owl_pos[1]+5))
+    screen.blit(font2.render(owl_text, True, BLACK), (dialog_owl_pos[0] + 5, dialog_owl_pos[1] + 5))
     pygame.display.update()
     pygame.time.wait(2000)
+
 
 run = True
 while run:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             run = False
-        elif e.type == pygame.KEYDOWN: 
+        # выход из программы при нажатии на клавищу ESCAPE
+        elif e.type == pygame.KEYDOWN:
             if e.key == pygame.K_ESCAPE:
                 run = False
-            elif e.unicode.isdecimal() and block==0:
+            # печатает только цифры
+            elif e.unicode.isdecimal() and block == 0:
                 numeral += e.unicode
+            elif e.key == pygame.K_BACKSPACE:
+                numeral = numeral[:-1]
+            elif e.key == pygame.K_RETURN and numeral:
+                if int(numeral) > 100:
+                    dialogs("",OUTSIDE_BG,'вы ошиблись')
+                elif int(numeral) > num:
+                    dialogs("",OUTSIDE_BG,'Бери меньше жмот')
+                elif int(numeral) < num:
+                    dialogs("",OUTSIDE_BG,'Чё так мало')
+                if move == 1:
+                    if int(numeral) == num:
+                        dialogs(f'Это число{numeral}', dialog_cat_pos,'Кiт, ты выиграл')
+                        block=1
+                    else:
+                        dialogs('Пёс твой ход', dialog_cat_pos,'Продолжаем')
+                elif move == 2:
+                    if int(numeral) == num:
+                        dialogs(f'Это число{numeral}', dialog_dog_pos,'Пёс, ты выиграл')
+                        block=1
+                    else:
+                        dialogs('Кiт твой ход', dialog_dog_pos,'Продолжаем')
+                numeral = ''
+                move += 1
+                if move > 2:
+                    move = 1
     if block == 0:
         screen.blit(bg, bg_rect)
         screen.blit(cat, cat_rect)
@@ -67,11 +95,12 @@ while run:
         screen.blit(font_box, font_rect)
         font_box.fill(SILVER)
         font_box.blit(
-            font.render(numeral,True, BLACK),(10,0)
+            font.render(numeral, True, BLACK), (10, 0)
         )
     pygame.display.update()
+
     if start == 1:
-        dialog('',OUTSIDE_BG,'Я загадала число')
-        dialog('',OUTSIDE_BG,'от 0 до 100')
-        dialog('Кот твой ход',dialog_dog_pos,'Отгдайте его')
+        dialogs('', OUTSIDE_BG, 'Я загадала число')
+        dialogs('', OUTSIDE_BG, 'от 0 до 100')
+        dialogs('Кот твой ход', dialog_dog_pos, 'Отгадайте его')
         start = 0
